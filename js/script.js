3733920b4c5d003938149b2d4895d78ee1b53583
Leith Green <leithgwrk22@gmail.com>
2024-04-10 14:06:08 +0100

    add array of cocktail objects
    add an array of objects detailing 3 cocktails, their strengths and ingredients.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-alert('Hello World');
-
-let favoriteFood = 'burrito';
-document.write(favoriteFood);
+let cocktailList = [
+    {name: 'Margarita', strength: '8.5/10', ingredients: ['25ml Lime', '40ml Tequila', '30ml Cointreau']},
+    {name: 'Negroni', strength: '7/10', ingredients: ['30ml Gin', '30ml Dry Vermouth', '30ml Campari']},
+    {name: 'Strawberry Daiquiri', strength: '6/10', ingredients: ['50ml white Rum', '25ml Lime', '40ml strawberry puree']},
+];
